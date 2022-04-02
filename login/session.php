436f35678e3151b1b74fd7099744cd5999a0f202
Maicon <maicon.pzo@gmail.com>
2022-04-02 11:10:12 -0300

    ajuste simples

--- login/session.php
+++ login/session.php
@@ -2,10 +2,11 @@
 //require_once('../app/entities/conexao.php');
 $login = $_POST['login'];
 $senha = $_POST['senha'];
+var_dump($_POST);
 
-if ($login == 'admin' && $senha == 'admin') {
+/* if ($login == 'admin' && $senha == 'admin') {
     echo json_encode('aaaaaa');
 } else {
     return json_encode('bbbbbb');
     echo json_encode('cccc');
-}
+} */
